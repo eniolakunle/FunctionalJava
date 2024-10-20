144ae155affd9ebb9ef030317a4eeec6e5b2555f
Kunle Eniola <eniolakunle10@gmail.com>
2024-10-19 20:45:31 -0400

    exercise 3

---
+++ src/ex3/Exercise3.java
@@ -0,0 +1,18 @@
+package ex3;
+
+import dinosaurs.Dinosaur;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        int dinoCount = 0;
+        // variable must be final or effectively final (not allowed to change), this does not compile.
+        Consumer<List<Dinosaur>> increaseDino = list -> {
+            list.add(new Dinosaur("Dino" + dinoCount));
+            dinoCount++;
+        };
+    }
+}
