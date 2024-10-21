73c4b16f4dd38d3bbe45ad4f0a03dd9b134b87cd
Kunle Eniola <eniolakunle10@gmail.com>
2024-10-21 17:58:46 -0400

    clarification from Maaike

--- src/ex3/Exercise3.java
+++ src/ex3/Exercise3.java
@@ -2,17 +2,23 @@ package ex3;
 
 import dinosaurs.Dinosaur;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        int dinoCount = 0;
-        // variable must be final or effectively final (not allowed to change), this does not compile.
+        int[] dinoCount = {0}; // is allowed as value/reference does not change, but the state of the reference does.
+//        int dinoCount = 0; //variable must be final or effectively final (not allowed to change), this does not compile.
         Consumer<List<Dinosaur>> increaseDino = list -> {
             list.add(new Dinosaur("Dino" + dinoCount, 45));
-            dinoCount++;
+            dinoCount[0]++;
+//            dinoCount++;
         };
-    }
-}
+        increaseDino.accept(new ArrayList<Dinosaur>());
+        System.out.println(dinoCount[0]);
+//        System.out.println(dinoCount);
+    }}
+
